7298b3c8f7590edf53ed941c302846583fb3248e
qlj <965024754@qq.com>
2022-10-22 12:11:15 +0800

    1

--- Assets/Scripts/SkillSystem/Deployers/BoomDeployer.cs
+++ Assets/Scripts/SkillSystem/Deployers/BoomDeployer.cs
@@ -6,20 +6,29 @@ namespace Game.Skill
 {
     public class BoomDeployer : SkillDeployer
     {
+        //炸弹物体的动画控制器
+        Animator anim;
+        Collider2D cd;
         bool isBoom;
         private void Start()
         {
+            cd.gameObject.SetActive(false);
             isBoom = false;
             DeploySkill();
         }
         public override void DeploySkill()
         {
-            
+            StartCoroutine(waitForBoom());
         }
 
         public IEnumerator waitForBoom()
         {
             yield return new WaitForSeconds(2f);
+            //播放动画
+
+            //等待指定帧数
+
+            //造成伤害
         }
     }
 }
