18de257813046843f9434761e0004260c30f74ea
qlj <965024754@qq.com>
2022-10-23 10:18:59 +0800

    小修改

--- Assets/Scripts/SkillSystem/Deployers/BoomDeployer.cs
+++ Assets/Scripts/SkillSystem/Deployers/BoomDeployer.cs
@@ -9,21 +9,23 @@ namespace Game.Skill
         //炸弹物体的动画控制器
         Animator anim;
         Collider2D cd;
-        bool isBoom;
+        Vector3 originalTrans;
         private void Start()
         {
+            originalTrans = gameObject.transform.position;
+            gameObject.transform.position = new Vector3(originalTrans.x, originalTrans.y - 0.5f, originalTrans.z);
             //cd.gameObject.SetActive(false);
-            isBoom = false;
             DeploySkill();
         }
         public override void DeploySkill()
         {
+            Destroy(gameObject, 5f);
             StartCoroutine(waitForBoom());
         }
 
         public IEnumerator waitForBoom()
         {
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(5f);
             //播放动画
 
             //等待指定帧数
