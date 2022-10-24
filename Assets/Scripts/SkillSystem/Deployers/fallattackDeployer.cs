14df0a0eaaefc99ea39bb46925a74ecc4cbb4abd
qlj <965024754@qq.com>
2022-10-24 16:37:58 +0800

    下踹

---
+++ Assets/Scripts/SkillSystem/Deployers/fallattackDeployer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Skill
+{
+    public class fallattackDeployer : SkillDeployer
+    {
+        private void Start()
+        {
+            DeploySkill();
+        }
+        public override void DeploySkill()
+        {
+
+        }
+    }
+}
