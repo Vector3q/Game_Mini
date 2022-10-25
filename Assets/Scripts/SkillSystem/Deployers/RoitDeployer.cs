8483ac6e1ff3f1780b954daf1d62ab555fb56e2d
qlj <965024754@qq.com>
2022-10-25 17:00:35 +0800

    拳击

--- Assets/Scripts/SkillSystem/Deployers/RoitDeployer.cs
+++ Assets/Scripts/SkillSystem/Deployers/RoitDeployer.cs
@@ -7,12 +7,16 @@ namespace Game.Skill
     {
         private void Start()
         {
-           
+            
             DeploySkill();
+        }
+        private void Update()
+        {
+            gameObject.transform.position = new Vector3(SkillData.owner.transform.position.x+1f, SkillData.owner.transform.position.y+3f, SkillData.owner.transform.position.z);
         }
         public override void DeploySkill()
         {
-            
+            Destroy(gameObject, 1f);
         }
     }
 }
