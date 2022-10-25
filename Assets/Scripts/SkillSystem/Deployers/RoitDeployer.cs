81c5c51c5d74a925c57ceb5cec391a80228aa37b
qlj <965024754@qq.com>
2022-10-25 19:14:22 +0800

    射日完成了捏

--- Assets/Scripts/SkillSystem/Deployers/RoitDeployer.cs
+++ Assets/Scripts/SkillSystem/Deployers/RoitDeployer.cs
@@ -5,18 +5,36 @@ namespace Game.Skill
 {
     public class RoitDeployer : SkillDeployer
     {
+        float timer;
+        bool flag = false;
         private void Start()
         {
-            
             DeploySkill();
         }
         private void Update()
         {
-            gameObject.transform.position = new Vector3(SkillData.owner.transform.position.x+1f, SkillData.owner.transform.position.y+3f, SkillData.owner.transform.position.z);
+            if(flag)
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, SkillData.owner.transform.position.y + (3 + 10 * timer), gameObject.transform.position.z);
+            }
+            timer += 0.01f;
+            Debug.Log(timer);
         }
         public override void DeploySkill()
         {
-            Destroy(gameObject, 1f);
+            
+            gameObject.transform.position = new Vector3(SkillData.owner.transform.position.x, SkillData.owner.transform.position.y - 10f, SkillData.owner.transform.position.z);
+            StartCoroutine(wait());
+            Destroy(gameObject, 3f);    
+        }
+
+        private IEnumerator wait()
+        {
+            
+            yield return new WaitForSeconds(0.4f);
+            flag = true;
+            gameObject.transform.position = new Vector3(SkillData.owner.transform.position.x + (SkillData.owner.transform.localScale.x/1.4f) * 1f, SkillData.owner.transform.position.y + 3f, SkillData.owner.transform.position.z);
+            timer = 0;
         }
     }
 }
